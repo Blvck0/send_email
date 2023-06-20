e6f8b1bb2390ff94e93562d4756c96b2b4db83cc
kaiser olajuwon <kaiserjaey@gmail.com>
2023-06-20 03:25:22 +0100

    created the sendEmail function adding the parameters and also the server functions.

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
 
 
 import smtplib
+
+def sendEmail(to, content):
+    server = smtplib.SMTP('smtp.gmail.com', 587)
+    server.ehlo()
+    server.starttls()
+    server.login('your email', 'your password')
+    server.sendmail('your email',to,content)
+    server.close()
+    
