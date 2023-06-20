b0169946e7c7f40c62d61f92499eb9f9eaa48557
kaiser olajuwon <kaiserjaey@gmail.com>
2023-06-20 03:38:33 +0100

    I created an if function for the name. and handling errors using the try block.

--- main.py
+++ main.py
@@ -6,7 +6,17 @@ def sendEmail(to, content):
     server = smtplib.SMTP('smtp.gmail.com', 587)
     server.ehlo()
     server.starttls()
-    server.login('your email', 'your password')
-    server.sendmail('your email',to,content)
+    server.login('kaiserjaey@gmail.com','duhiyllyuimdzpcr')
+    server.sendmail('kaiserjaey@gmail.com',to,content)
     server.close()
-    
+
+
+if __name__ == '__main__':
+    try:
+        content = input('Type your text \n')
+        to = input('receiver\'s email')
+        sendEmail(to, content)
+        print('Email sent successfully')
+    except Exception as e:
+        print(e)
+        print('Email not sent')
